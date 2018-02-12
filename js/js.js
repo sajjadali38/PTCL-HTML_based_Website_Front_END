e3aedbe09fea3cf17e4a3c0d14565bb73f76177f
Sajjad <sajjadali38>
2018-02-12 16:42:35 +0100

    First commit

---
+++ js/js.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+function validate_form()
+{
+  if(demo.Name.value.length=="0")
+  {
+    alert("plz enter ur name");
+	}
+ if(demo.FatherName.value.length=="0")
+  {
+    alert("plz enter ur Father name");
+	}
+
+	 if(demo.Postalcode.value.length=="0")
+  {
+    alert("plz enter ur Postal code");
+	}
+	 if(demo.email.value.length=="0")
+  {
+    alert("plz enter ur Email");
+	}
+	
+  }
+  
+  
+  
